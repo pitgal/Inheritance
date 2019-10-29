288142a2f000eb70d09fb824c201ca632f7d3793
pitgal <pitgal@gmail.com>
2019-10-29 23:30:07 +0100

    Zmiany

--- main.cpp
+++ main.cpp
@@ -1,55 +1,5 @@
-#include <iostream>
-using namespace std;
- 
-struct ISwitchable {
-	virtual void On() = 0;
-	virtual void Off() = 0;
-};
- 
-class Switch {
-  bool state = false;
-  ISwitchable& switchable;
- 
-public:
-  Switch(ISwitchable& _switchable) : switchable(_switchable) {}
-  void Toggle() {
-  	if (state)
-  	  switchable.Off();
-  	else 
-  	  switchable.On();
- 
-  	state = !state;  
-  }
-};
- 
-class Lamp :public ISwitchable {
-	bool lighting = false;
- 
-public:
-  void On() override {
-  	lighting = true;
-  	Print();
-  }
-  void Off() override {
-  	lighting = false;
-  	Print();
-  }
-  virtual void Print() const {
-  	std::cout << "Lamp " << (IsLighting() ? "on" : "off") << endl;
-  }
-  virtual bool IsLighting() const { 
-  	return lighting;
-  };
-};
- 
-class BigLamp :public Lamp {
-  virtual void Print() const override {
-    Lamp::Print();
-    cout << "BigLamp\n"; 
-  }	
-};
-
- 
+#include "lamp.h"
+  
 int main() {
 	BigLamp lampa;
 	Switch wlacznik(lampa);
